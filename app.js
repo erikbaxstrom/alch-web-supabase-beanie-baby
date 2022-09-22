bf5dee9da6eac8b80a96180ecdf58ec5b3f58603
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-22 10:46:25 -0700

    add basic supabase connection

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 /* Imports */
-
+import { getBeanies } from './fetch-utils.js';
 /* Get DOM Elements */
-
+const cardsSection = document.getElementById('cards-section');
 /* State */
 
 /* Events */
 
+window.addEventListener('load', () => {
+    findBeanies();
+});
 /* Display Functions */
 
+async function findBeanies() {
+    const response = await getBeanies();
+    console.log('response:', response);
+    // error = response.error;
+    // beanies = response.data;
+    // displayBeanies();
+}
 // (don't forget to call any display functions you want to run on page load!)
